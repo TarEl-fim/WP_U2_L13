1f74e51ded8b5c5dcd48f9af61775bddf25f3b29
Tarrah <tarrah.elash@gmail.com>
2025-11-18 10:46:54 -0500

    Added template to JS and changed html to body/div

---
+++ pages/index/index.js
@@ -0,0 +1,8 @@
+/*<div class="invisi">
+            <div class="card"></div>
+            <div class="card"></div>
+            <div class="card"></div>
+            <div class="card"></div>
+            <div class="card"></div>
+        </div> 
+^^ card generation template ^^*/
